fdbce33d74e37cbb59dc11eb2fc2ef0bbe1faf18
Kacper Kurzawski <kurzawa.sc@gmail.com>
2026-08-06 15:32:37 +0200

    feat: dark motive

--- MuscleMemory/App.xaml.cs
+++ MuscleMemory/App.xaml.cs
@@ -7,6 +7,13 @@ namespace MuscleMemory
         public App()
         {
             InitializeComponent();
+            string savedTheme = Preferences.Default.Get("AppTheme", "System");
+            UserAppTheme = savedTheme switch
+            {
+                "Light" => AppTheme.Light,
+                "Dark" => AppTheme.Dark,
+                _ => AppTheme.Unspecified
+            };
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
